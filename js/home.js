afafc0b567394c99818ae532eaa23019274873fb
MiltonKienigiel <miltonkienigiel@gmail.com>
2020-05-15 11:11:04 -0300

    Agregue Js para Header en el celu, no funciona todavia

---
+++ js/home.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function(){
+    var menu = document.querySelector("toggle-menu");
+
+    var navegacion = document.querySelector("div.main-nav2");
+    
+    menu.addEventListener("click", function(){
+        if( navegacion.style.display == "none"){
+            navegacion.style.display = "block";
+        }else{
+            navegacion.style.display = "none";
+        }
+    
+    })
+    
+})
+

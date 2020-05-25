e153ff9b9ecb60e61e84c97e3ce6fe317db54aa3
MiltonKienigiel <miltonkienigiel@gmail.com>
2020-05-25 04:30:48 -0300

    Cambie Headers en todas las paginas

--- js/general.js
+++ js/general.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function(){
     var menu = document.querySelector(".toggle-menu");
 
-    var navegacion = document.querySelector("div.main-nav2");
+    var navegacion = document.querySelector(".contenedor-header");
     
     menu.addEventListener("click", function(){
         if( navegacion.style.display == "none"){

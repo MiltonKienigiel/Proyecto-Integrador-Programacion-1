66ab379d02132d8cba4282aac0ab88aa78187d08
MiltonKienigiel <miltonkienigiel@gmail.com>
2020-06-06 05:44:06 -0300

    Agregar en playlist Cartel que hay NULL

--- js/playlist.js
+++ js/playlist.js
@@ -6,19 +6,23 @@ console.log(tracks);
 
 let contenedorPlaylist = document.querySelector(".playlist");
 
-tracks.forEach(track => {
-    
-    contenedorPlaylist.innerHTML +=`
-    
-    
-<div class="song"> 
-    <p> <audio class="audios" src="${track.preview}" controls ></audio></p>
-    <p><a href="detalles-tracks.html?id=${track.id}" class="songArtista" >${track.title_short}</a></p>
-    <p class="d-none">${track.album.title}</p> 
-    <p><a href="details.html?id=${track.id}" class="songArtista" >${track.artist.name}</a></p>
-</div>
-    
-    `
-});
+if(tracks === null){
 
+}else{
+
+    tracks.forEach(track => {
+    
+        contenedorPlaylist.innerHTML +=`
+        
+        
+    <div class="song"> 
+        <p> <audio class="audios" src="${track.preview}" controls ></audio></p>
+        <p><a href="detalles-tracks.html?id=${track.id}" class="songArtista" >${track.title_short}</a></p>
+        <p class="d-none">${track.album.title}</p> 
+        <p><a href="details.html?id=${track.id}" class="songArtista" >${track.artist.name}</a></p>
+    </div>
+        
+        `
+    });
+}
 }
